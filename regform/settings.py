61410c767e49af8d74bf827022b1c96066ec8dea
maja-98 <majajouhar@gmail.com>
2022-07-21 11:11:58 -0500

    release fix

--- regform/settings.py
+++ regform/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-jdcs5qrl9!gsx!d3mu=w48k0l2ryv8m89a4$w4(e%7vt*!6k0j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['regform98.herokuapp.com']
 
 
 # Application definition
