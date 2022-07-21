694b1aad5c93c61f4730db7d6da413fcf4d0bd90
Muhammed <jmuha@acehardware.com>
2022-07-21 10:17:28 -0500

    Automatic configuration with django-heroku

--- regform/settings.py
+++ regform/settings.py
@@ -126,5 +126,5 @@ MEDIA_URL='/images/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-"import django_heroku" 
-"django_heroku.settings(local())" 
+import django_heroku
+django_heroku.settings(locals())
