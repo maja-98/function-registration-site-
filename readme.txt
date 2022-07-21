8521b2606e11ce240b8eebd83ae2536f0365403a
Muhammed <jmuha@acehardware.com>
2022-07-21 10:11:13 -0500

    Automatic configuration with django-heroku

--- readme.txt
+++
@@ -1,4 +0,0 @@
-django == 4.0.6
-Pillow == 9.2.0
-
-https://realpython.com/django-hosting-on-heroku/
--- regform/settings.py
+++ regform/settings.py
@@ -126,3 +126,5 @@ MEDIA_URL='/images/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+"import django_heroku" 
+"django_heroku.settings(local())" 
