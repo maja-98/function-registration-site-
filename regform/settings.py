5c2aaa9fc48d19e6c05b0a807398b2e9cbfe01a9
Muhammed <jmuha@acehardware.com>
2022-07-21 10:46:07 -0500

    Remove a hardcoded Django secret key

--- regform/settings.py
+++ regform/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jdcs5qrl9!gsx!d3mu=w48k0l2ryv8m89a4$w4(e%7vt*!6k0j'
+#SECRET_KEY = 'django-insecure-jdcs5qrl9!gsx!d3mu=w48k0l2ryv8m89a4$w4(e%7vt*!6k0j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
